eccfad9680e346d6392e3deb2d56024b407033c4
saikishoreB <45278993+saikishoreB@users.noreply.github.com>
2018-11-30 13:33:09 +0530

    Add files via upload

---
+++ src/main/java/com/ecomm/model/Category.java
@@ -0,0 +1,43 @@
+package com.ecomm.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Category {
+
+	@Id
+	int categoryId;
+	String categoryName;
+	String categorydesc;
+	//@OneToMany(mappedBy="category")
+	//private List<Product> products;
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	/*public List<Product> getProducts() {
+		return products;
+	}
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}*/
+	public String getCategorydesc() {
+		return categorydesc;
+	}
+	public void setCategorydesc(String categorydesc) {
+		this.categorydesc = categorydesc;
+	}
+	
+}
